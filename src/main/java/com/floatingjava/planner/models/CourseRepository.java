ac1836745faa2128b2a6880f1aa4554efe21a29d
JamesDansie <jdansie8336@gmail.com>
2019-10-15 15:05:25 -0700

    added labels and dropdown dates

--- src/main/java/com/floatingjava/planner/models/CourseRepository.java
+++ src/main/java/com/floatingjava/planner/models/CourseRepository.java
@@ -2,5 +2,8 @@ package com.floatingjava.planner.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    public List<Course> findAllByOrderByStartDateAsc();
 }
