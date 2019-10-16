dae00d4b56b1baf4856b4663bd295eb029c9e324
Jon Veach <veach09@gmail.com>
2019-10-16 13:38:19 -0700

    Built crud operations with ed plans

---
+++ src/main/java/com/floatingjava/planner/models/DateSavedRepository.java
@@ -0,0 +1,9 @@
+package com.floatingjava.planner.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface DateSavedRepository extends JpaRepository<DateSaved, Long> {
+    @Query(value = "update date_saved set created_at_minutes = ?1", nativeQuery = true)
+    public void updatedOurTime(long time);
+}
