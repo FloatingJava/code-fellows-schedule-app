ce19bf8607e99e30beb31f8dba0311f20b0bbd11
James Dansie <45674851+JamesDansie@users.noreply.github.com>
2019-10-16 09:33:39 -0700

    Gantt format (#25)
    * working gantt
    
    * turned off app.js for now

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,12 +4,16 @@
 
 
 // console.log('hi');
+
+// //Getting JSON 
 // const requestURL = 'https://s3-us-west-2.amazonaws.com/static.codefellows.org/courses/schedule.json';
 // var request = new XMLHttpRequest();
 // request.open('Get', requestURL);
 // requestURL.responseType = 'json';
 // request.send();
 // request.onload = function() {
+
+//   //Parsing JSON 
 //   const courseJSONstr = request.response;
 //   const courseJSONobj = JSON.parse(courseJSONstr);
 //   let courseArr = [];
@@ -17,16 +21,18 @@
 //   courseArr = courseJSONobj.courses
 //   courseArr.forEach(course => {
 //     calendarElement = [];
-//     console.log('***************************')
-//     console.log(course)
-//     calendarElement.push(course.code);
-//     calendarElement.push(course.code);
-//     calendarElement.push(new Date(course.startDate));
-//     calendarElement.push(new Date(course.endDate));
+
+//     //Converting data types
+//     calendarElement.push(course.course.code);
+//     calendarElement.push(course.course.code);
+//     calendarElement.push(new Date(Date.parse(course.course.startDate)));
+//     calendarElement.push(new Date(Date.parse(course.course.endDate)));
+//     calendarElement.push(null);
+//     calendarElement.push(null);
+//     calendarElement.push(null);
+
 //     calendarArr.push(calendarElement);
 //   })
-//   console.log(courseJSONobj.courses);
-
 
 //   // ******** Chart Land ***********
 //   google.charts.load('current', {'packages':['gantt']});
@@ -43,23 +49,14 @@
 //     data.addColumn('string', 'Course Name');
 //     data.addColumn('date', 'Start Date');
 //     data.addColumn('date', 'End Date');
+//     data.addColumn('number', 'Duration');
+//     data.addColumn('number', 'Percent Complete');
 //     data.addColumn('string', 'Dependencies');
 
-//     data.addRows([
-//       ['Research', 'Find sources',
-//       new Date(2015, 0, 1), new Date(2015, 0, 5), null],
-//       ['Write', 'Write paper',
-//       null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
-//       ['Cite', 'Create bibliography',
-//       null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
-//       ['Complete', 'Hand in paper',
-//       null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
-//       ['Outline', 'Outline paper',
-//       null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
-//     ]);
+//     data.addRows(calendarArr);
 
 //     var options = {
-//       height: 275
+//       height: 2000,
 //     };
 
 //     var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
@@ -69,43 +66,44 @@
 // }
 
 
-// ******** Chart Land ***********
-google.charts.load('current', {'packages':['gantt']});
-google.charts.setOnLoadCallback(drawChart);
+// // ******** Chart Land Original ***********
 
-function daysToMilliseconds(days) {
-  return days * 24 * 60 * 60 * 1000;
-}
+// google.charts.load('current', {'packages':['gantt']});
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
+// function daysToMilliseconds(days) {
+//   return days * 24 * 60 * 60 * 1000;
+// }
 
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Task ID');
-  data.addColumn('string', 'Task Name');
-  data.addColumn('date', 'Start Date');
-  data.addColumn('date', 'End Date');
-  data.addColumn('number', 'Duration');
-  data.addColumn('number', 'Percent Complete');
-  data.addColumn('string', 'Dependencies');
+// function drawChart() {
 
-  data.addRows([
-    ['Research', 'Find sources',
-     new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
-    ['Write', 'Write paper',
-     null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
-    ['Cite', 'Create bibliography',
-     null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
-    ['Complete', 'Hand in paper',
-     null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
-    ['Outline', 'Outline paper',
-     null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
-  ]);
+//   var data = new google.visualization.DataTable();
+//   data.addColumn('string', 'Task ID');
+//   data.addColumn('string', 'Task Name');
+//   data.addColumn('date', 'Start Date');
+//   data.addColumn('date', 'End Date');
+//   data.addColumn('number', 'Duration');
+//   data.addColumn('number', 'Percent Complete');
+//   data.addColumn('string', 'Dependencies');
 
-  var options = {
-    height: 275
-  };
+//   data.addRows([
+//     ['Research', 'Find sources',
+//      new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
+//     ['Write', 'Write paper',
+//      null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
+//     ['Cite', 'Create bibliography',
+//      null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
+//     ['Complete', 'Hand in paper',
+//      null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
+//     ['Outline', 'Outline paper',
+//      null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
+//   ]);
 
-  var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+//   var options = {
+//     height: 275
+//   };
 
-  chart.draw(data, options);
-}
+//   var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+
+//   chart.draw(data, options);
+// }
